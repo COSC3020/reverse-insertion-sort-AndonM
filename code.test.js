505306532e24f7d1379c3311a86a53cc52ea1e8c
Andon Mefford <andonmefford@outlook.com>
2023-12-05 13:19:05 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -4,9 +4,9 @@ eval(fs.readFileSync('code.js') + '');
 const testSort = jsc.forall("array nat", function (arr) {
     let a1 = JSON.parse(JSON.stringify(arr));
     let a2 = JSON.parse(JSON.stringify(arr));
-    a2.sort(function (a, b) {
+    let result = a2.sort(function (a, b) {
         return a - b;
     });
-    return JSON.stringify(insertionSortReverse(a1)) === JSON.stringify(a2);
+    return JSON.stringify(insertionSortReverse(a1)) === JSON.stringify(result);
 });
 jsc.assert(testSort);
