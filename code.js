18b7cd4c751a457727b38d623a413fe2a0792ddf
Andon Mefford <andonmefford@outlook.com>
2023-12-05 13:06:23 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,11 +1,16 @@
 function insertionSortReverse(arr) {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  else {
   for (let i = arr.length - 1; i >= 0; i--) {
     let val = arr[i];
     let j;
     for (j = i; j < arr.length && arr[j] > val; j++) {
       arr[j] = arr[j - 1];
-    }
+      }
     arr[j] = val;
-  }
+    }
   return arr;
+  }
 }
