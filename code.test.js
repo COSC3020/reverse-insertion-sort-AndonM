0445c24835f5426f673ca46540d7e419cb3946d5
Andon Mefford <andonmefford@outlook.com>
2023-12-05 12:53:36 -0700

    Update code.test.js

--- code.test.js
+++ code.test.js
@@ -1,15 +1,12 @@
 const fs = require('fs');
 const jsc = require('jsverify');
-
-eval(fs.readFileSync('code.js')+'');
-
-const testSort =
-    jsc.forall("array nat", function(arr) {
-        let a1 = JSON.parse(JSON.stringify(arr));
-        let a2 = JSON.parse(JSON.stringify(arr));
-        return JSON.stringify(insertionSortReverse(a1)) ==
-            JSON.stringify(a2.sort(function(a, b)
-                { return a - b; }));
+eval(fs.readFileSync('code.js') + '');
+const testSort = jsc.forall("array nat", function (arr) {
+    let a1 = JSON.parse(JSON.stringify(arr));
+    let a2 = JSON.parse(JSON.stringify(arr));
+    a2.sort(function (a, b) {
+        return b - a;
     });
-
+    return JSON.stringify(insertionSortReverse(a1)) === JSON.stringify(a2);
+});
 jsc.assert(testSort);
